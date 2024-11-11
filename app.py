90d25fa9b45c63a0de64d1f36780eb00d92a68ce
cesarperez0321 <79946216+cesarperez0321@users.noreply.github.com>
2024-11-11 11:14:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ CORS(app)  # Habilita CORS en la aplicación
 
 @app.route('/hello', methods=['GET'])
 def hello_world():
-    return jsonify(message="Hello World")
+    return jsonify(message="Hello World-Educare.ai")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
